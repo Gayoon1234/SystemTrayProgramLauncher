4c410e678d6d56965f28c022a17bb4c8291785f2
Gayoon1234 <s3843584@student.rmit.edu.au>
2023-03-12 15:56:26 +1100

    Added some hardcoded Context Menu items

--- SystemTrayProgramLauncher/SystemTrayProgramLauncher/Form1.Designer.cs
+++ SystemTrayProgramLauncher/SystemTrayProgramLauncher/Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.option1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.single2ExtendToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extend2SingleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.oneDriveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,15 +54,60 @@
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.option1ToolStripMenuItem});
+            this.option1ToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.oneDriveToolStripMenuItem,
+            this.test1ToolStripMenuItem,
+            this.test2ToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(118, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 120);
             // 
             // option1ToolStripMenuItem
             // 
+            this.option1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.single2ExtendToolStripMenuItem,
+            this.extend2SingleToolStripMenuItem});
             this.option1ToolStripMenuItem.Name = "option1ToolStripMenuItem";
-            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(117, 22);
-            this.option1ToolStripMenuItem.Text = "Option1";
+            this.option1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.option1ToolStripMenuItem.Text = "Display";
+            // 
+            // single2ExtendToolStripMenuItem
+            // 
+            this.single2ExtendToolStripMenuItem.Name = "single2ExtendToolStripMenuItem";
+            this.single2ExtendToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.single2ExtendToolStripMenuItem.Text = "Extend";
+            this.single2ExtendToolStripMenuItem.Click += new System.EventHandler(this.single2ExtendToolStripMenuItem_Click);
+            // 
+            // extend2SingleToolStripMenuItem
+            // 
+            this.extend2SingleToolStripMenuItem.Name = "extend2SingleToolStripMenuItem";
+            this.extend2SingleToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.extend2SingleToolStripMenuItem.Text = "Single";
+            this.extend2SingleToolStripMenuItem.Click += new System.EventHandler(this.extend2SingleToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // oneDriveToolStripMenuItem
+            // 
+            this.oneDriveToolStripMenuItem.Name = "oneDriveToolStripMenuItem";
+            this.oneDriveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.oneDriveToolStripMenuItem.Text = "OneDrive";
+            this.oneDriveToolStripMenuItem.Click += new System.EventHandler(this.oneDriveToolStripMenuItem_Click);
+            // 
+            // test1ToolStripMenuItem
+            // 
+            this.test1ToolStripMenuItem.Name = "test1ToolStripMenuItem";
+            this.test1ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test1ToolStripMenuItem.Text = "Test1";
+            // 
+            // test2ToolStripMenuItem
+            // 
+            this.test2ToolStripMenuItem.Name = "test2ToolStripMenuItem";
+            this.test2ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test2ToolStripMenuItem.Text = "Test2";
             // 
             // Form1
             // 
@@ -76,5 +127,11 @@
         private NotifyIcon notifyIcon;
         private ContextMenuStrip contextMenuStrip1;
         private ToolStripMenuItem option1ToolStripMenuItem;
+        private ToolStripMenuItem single2ExtendToolStripMenuItem;
+        private ToolStripMenuItem extend2SingleToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem oneDriveToolStripMenuItem;
+        private ToolStripMenuItem test1ToolStripMenuItem;
+        private ToolStripMenuItem test2ToolStripMenuItem;
     }
 }
--- SystemTrayProgramLauncher/SystemTrayProgramLauncher/Form1.cs
+++ SystemTrayProgramLauncher/SystemTrayProgramLauncher/Form1.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace SystemTrayProgramLauncher
@@ -30,5 +31,43 @@ namespace SystemTrayProgramLauncher
                 contextMenuStrip1.Show(Cursor.Position);
             }
         }
+
+        //FYI, DisplaySwitch.exe is included in the bin file as MS messed up the WIN11 implementation of it.
+        private void single2ExtendToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string baseDirectory = AppContext.BaseDirectory;
+            string displayswitchPath = Path.Combine(baseDirectory, "Scripts", "DisplaySwitch.exe");
+            Process.Start(displayswitchPath, "/extend");
+        }
+
+        private void extend2SingleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string baseDirectory = AppContext.BaseDirectory;
+            string displayswitchPath = Path.Combine(baseDirectory, "Scripts", "DisplaySwitch.exe");
+            Process.Start(displayswitchPath, "/internal");
+        }
+
+        //This will only work on my PC.
+        private void oneDriveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string scriptPath = @"C:\Users\gayan\OneDrive - RMIT University\2023\Notes\word2pdf.ps1";
+            ProcessStartInfo psi = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-File \"{scriptPath}\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+            };
+            Process.Start(psi);
+        }
+
+
+        //Code for later
+
+        //Basically the plan is to allow users to create custom Context menu items/
+
+        //ToolStripMenuItem newItem = new ToolStripMenuItem("New Menu Item");
+        //newItem.Click += new EventHandler(newItem_Click);
+        //contextMenuStrip1.Items.Add(newItem);
     }
 }
